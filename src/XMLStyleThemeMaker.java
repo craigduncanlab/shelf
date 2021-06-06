373154c94a5c175922f01240db07ff857f21b0b6
Craig Duncan <software@craigduncan.com.au>
2021-06-06 22:37:31 +0800

    add functions for adding sets of style-based fields to current docx

--- src/XMLStyleThemeMaker.java
+++ src/XMLStyleThemeMaker.java
@@ -54,6 +54,8 @@ public void addMDStyles() {
     Boolean noteTest=false;
     Boolean codeTest=false;
     Boolean nrTest=false;
+    Boolean eventTest=false;
+    int failedTests=0;
     for (xstyle aStyle : myUStyleList) {
        String thisId=aStyle.getId();
        //System.out.println(thisId);
@@ -63,62 +65,211 @@ public void addMDStyles() {
        if (thisId.equals("Code")) {
         codeTest=true;
        }
-       if (thisId.equals("NormalReally")) {
+       if (thisId.equals("RecordHeading")) {
         nrTest=true;
+       }
+       if (thisId.equals("Event")) {
+        eventTest=true;
        }
     }
     
     if (codeTest==false){
       addCodeStyle();
+      failedTests++;
+    }
+    if (nrTest==false){
+      addNormalRecordStyle();
+      failedTests++;
     }
     if (noteTest==false){
       addNoteStyle();
+      failedTests++;
     }
-    if (nrTest==false){
-      addNormalReallyStyle();
-
+    if (eventTest==false){
+    	failedTests++;
+    	addEventStyle();
     }
     //update styles if necessary
-    if (codeTest==false || noteTest==false || nrTest==false) {
+    if (codeTest==false || noteTest==false || nrTest==false ) {
       //put details into new object
     updatedXMLStyles.recreateStylesFile();
 
-    myUStyleList = getUpdatedStylesXML().getStyleObjects();
+    //myUStyleList = getUpdatedStylesXML().getStyleObjects();
 	}
 }
 
 //TO DO: custom parameter setter for xstyle objects
 //<w:aliases w:val="RCode,PythonCode"/>
 ////Source Code F8F8F8 not 00E2AA
+//shad can be a run or a paragraph property
+
+//id, BasedOn,afterPara,outline,font,size,shade, afterPts,color
 private void addCodeStyle(){
-  String codeStyle = "<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Code\"><w:name w:val=\"Code\"/><w:aliases w:val=\"RCode,PythonCode\"/><w:basedOn w:val=\"Indent1\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\"F8F8F8\"/></w:pPr><w:rPr><w:i/><w:rFonts ascii=\"Consolas\" w:hAnsi=\"Consolas\"</w:rPr></w:style>";
+  
   xstyle newStyle = new xstyle();
-  newStyle.setStyleXML(codeStyle);
+  newStyle.setStyleAttrib("Code","Normal","",4,"Consolas",22,"shade",0,"",""); 
+  
+  String test = "<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Code\"><w:name w:val=\"Code\"/><w:basedOn w:val=\"Normal\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:outlineLvl w:val=\"4\"/><w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\"F8F8F8\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Consolas\" w:hAnsi=\"Consolas\"/><w:sz w:val=\"22\"/></w:rPr></w:style>";
+  if (!newStyle.getStyleXML().equals(test)){
+  	System.out.println("Test:");
+  	System.out.println(test);
+  	System.out.println("Failed:");
+  	System.out.println(newStyle.getStyleXML());
+  	System.exit(0);
+  }
+  //newStyle.setStyleXML(codeStyle);
   updatedXMLStyles.addStyle(newStyle); //add to current list of styles.
-  //System.out.println("Code style added");
-  //System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
 }
 
+//id, BasedOn,afterPara,outline,font,size,shade, afterPts,color
 private void addNoteStyle(){
   String codeStyle = "<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Note\"><w:name w:val=\"Note\"/><w:basedOn w:val=\"Indent1\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\"E3D200\"/></w:pPr><w:rPr><w:i/></w:rPr><w:outlineLvl w:val=\"4\"/></w:style>";
   xstyle newStyle = new xstyle();
-  newStyle.setStyleXML(codeStyle);
+  newStyle.setStyleAttrib("Note","Normal","",4,"Times New Roman",23,"note",0,"",""); //id, outline,font,size, shade
   updatedXMLStyles.addStyle(newStyle); //add to current list of styles.
-  //System.out.println("Note style added");
-  //System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
 }
-private void addNormalReallyStyle() {
-  /*
-  String codeStyle="<w:style w:type=\"paragraph\" w:styleId=\"NormalReally\"><w:name w:val=\"NormalReally\"/><w:basedOn w:val=\"Normal\"/><w:uiPriority w:val=\"0\"/><w:qFormat/><w:pPr><w:jc w:val=\"both\"/><w:outlineLvl w:val=\"4\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Times New Roman\" w:eastAsia=\"Times New Roman\" w:hAnsi=\"Times New Roman\" w:cs=\"Times New Roman\"/><w:sz w:val=\"22\"/><w:lang w:bidi=\"ar-SA\"/></w:rPr></w:style>";
-  */
+
+public void addEventStyle() {
+   String test="<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Event\"><w:name w:val=\"Event\"/><w:basedOn w:val=\"Heading1\"/><w:next w:val=\"Normal\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:outlineLvl w:val=\"0\"/><w:spacing w:after=\"120\"/></w:pPr><w:rPr><w:color w:val=\"2F5496\"/><w:sz w:val=\"32\"/></w:rPr></w:style>";
   xstyle newStyle = new xstyle();
-  newStyle.setStyleAttrib("NormalReally",4,"Times New Roman",10,""); //id, outline,font,size, shade
+  newStyle.setStyleAttrib("Event","Heading1","Normal",0,"",32,"",120,"blue",""); 
+  if (!newStyle.getStyleXML().equals(test)){
+  	System.out.println("Test:");
+  	System.out.println(test);
+  	System.out.println("Failed:");
+  	System.out.println(newStyle.getStyleXML());
+  	System.exit(0);
+  }
+  updatedXMLStyles.addStyle(newStyle); //add to current list of styles.
+}
+
+private void addNormalRecordStyle() {
+
+  String test="<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"RecordHeading\"><w:name w:val=\"RecordHeading\"/><w:basedOn w:val=\"Normal\"/><w:next w:val=\"Normal\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:outlineLvl w:val=\"0\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Times New Roman\" w:hAnsi=\"Times New Roman\"/><w:b/><w:sz w:val=\"22\"/></w:rPr></w:style>";
+  xstyle newStyle = new xstyle();
+  newStyle.setStyleAttrib("RecordHeading","Normal","Normal",0,"Times New Roman",22,"",0,"","bold"); 
+  if (!newStyle.getStyleXML().equals(test)){
+  	System.out.println("Test:");
+  	System.out.println(test);
+  	System.out.println("Failed:");
+  	System.out.println(newStyle.getStyleXML());
+  	System.exit(0);
+  }
   updatedXMLStyles.addStyle(newStyle); //add to current list of styles (via XML string).
-  /*
-  System.out.println("Normal Really style added");
-  System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
-  System.exit(0);
-  */
+}
+
+
+public void addLetterStyles() {
+	ArrayList<xstyle> myStyleList = getCurrentStylesXML().getStyleObjects();
+	/*
+	System.out.println("These are the id's of all styles in cloned docx styles");
+	for (xstyle currentStyle : myStyleList) {
+	 		System.out.println(currentStyle.getId());
+	}
+	System.exit(0);
+	*/
+	updatedXMLStyles.setStylesXML(currentXMLStyles.getStylesXML());
+	ArrayList<xstyle> myUStyleList = getUpdatedStylesXML().getStyleObjects();
+	/*
+	ArrayList<xstyle> myUStyleList = getUpdatedStylesXML().getStyleObjects();
+	System.out.println("These are the id's of all styles in cloned docx styles");
+	for (xstyle updatedStyle : myUStyleList) {
+	 		System.out.println(updatedStyle.getId());
+	}
+	System.exit(0);
+	*/
+	String newStyles="";
+    Boolean dateTest=false;
+    Boolean writerTest=false;
+    Boolean addressTest=false;
+    Boolean subjectTest=false;
+    Boolean messageTest=false;
+    Boolean recipientTest=false;
+    int failedTests=0;
+    for (xstyle aStyle : myUStyleList) {
+       String thisId=aStyle.getId();
+       //System.out.println(thisId);
+       if (thisId.equals("Address")) {
+        addressTest=true;
+       }
+       if (thisId.equals("Recipient")) {
+        recipientTest=true;
+       }
+       if (thisId.equals("Writer")) {
+        writerTest=true;
+       }
+       if (thisId.equals("Date")) {
+        dateTest=true;
+       }
+       if (thisId.equals("SubjectLine")) {
+        subjectTest=true;
+       }
+       if (thisId.equals("Message")) {
+        messageTest=true;
+       }
+    }
+    
+    if (addressTest==false){
+      addLetterRecordStyle("Address");
+      failedTests++;
+    }
+    if (addressTest==false){
+      addLetterRecordStyle("Recipient");
+      failedTests++;
+    }
+    if (writerTest==false){
+      addLetterRecordStyle("Writer");
+      failedTests++;
+    }
+    if (dateTest==false){
+      addLetterRecordStyle("Date");
+      failedTests++;
+    }
+    if (subjectTest==false){
+      addBoldLetterRecordStyle("SubjectLine");
+      failedTests++;
+    }
+    if (messageTest==false){
+      addLetterRecordStyle("Message");
+      failedTests++;
+    }
+   
+    //update styles if necessary
+    if (failedTests>0) {
+    	//put details into new object
+    	updatedXMLStyles.recreateStylesFile();
+	}
+}
+
+//Does not set font or text size.  Based on Normal.
+private void addLetterRecordStyle(String styleName) {
+
+  String test="<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\""+styleName+"\"><w:name w:val=\""+styleName+"\"/><w:basedOn w:val=\"Normal\"/><w:next w:val=\"Normal\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:outlineLvl w:val=\"0\"/></w:pPr><w:rPr></w:rPr></w:style>";
+  xstyle newStyle = new xstyle();
+  newStyle.setStyleAttrib(styleName,"Normal","Normal",0,"",0,"",0,"",""); 
+  if (!newStyle.getStyleXML().equals(test)){
+  	System.out.println("Test:");
+  	System.out.println(test);
+  	System.out.println("Failed:");
+  	System.out.println(newStyle.getStyleXML());
+  	System.exit(0);
+  }
+  updatedXMLStyles.addStyle(newStyle); //add to current list of styles (via XML string).
+}
+
+private void addBoldLetterRecordStyle(String styleName) {
+
+  String test="<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\""+styleName+"\"><w:name w:val=\""+styleName+"\"/><w:basedOn w:val=\"Normal\"/><w:next w:val=\"Normal\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:outlineLvl w:val=\"0\"/></w:pPr><w:rPr><w:b/></w:rPr></w:style>";
+  xstyle newStyle = new xstyle();
+  newStyle.setStyleAttrib(styleName,"Normal","Normal",0,"",0,"",0,"","bold"); 
+  if (!newStyle.getStyleXML().equals(test)){
+  	System.out.println("Test:");
+  	System.out.println(test);
+  	System.out.println("Failed:");
+  	System.out.println(newStyle.getStyleXML());
+  	System.exit(0);
+  }
+  updatedXMLStyles.addStyle(newStyle); //add to current list of styles (via XML string).
 }
 
 
