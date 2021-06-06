f002409f780415569d58f14b8a54503676aa0d28
Craig Duncan <software@craigduncan.com.au>
2021-06-06 16:24:00 +0800

    moved some functions to ThemeMaker.  Made some methods private

--- src/xmlStyles.java
+++ src/xmlStyles.java
@@ -18,25 +18,33 @@ public xmlStyles(){
 	
 }
 
+//add a new style to existing Style List
+public void addStyle(xstyle input){
+  this.styleList.add(input);
+}
+
+private void updateSummaryStylesString() {
+  setOutlineNames(); //arg: ArrayList of xstyle. getStyles().  This is ALL styles.
+  makeSummaryStylesList(); 
+}
+
 //called externally by docXML when new Styles.xml is obtained
 public void setStylesXML(String input){
 	this.stylesString=input;
   convertStylesXMLtoObjects();
-  checkAndAddStyles();
-  setOutlineNames(); //arg: ArrayList of xstyle. getStyles().  This is ALL styles.
-  //makeSummaryStylesList(); 
-  //checkForNoteStyle();
-	//setLvl0Styles();
+  updateSummaryStylesString();
 }
-
-private void updateSummaryStylesString(String input) {
-  this.stylesString=input;
-  setOutlineNames(); //arg: ArrayList of xstyle. getStyles().  This is ALL styles.
-  makeSummaryStylesList(); //prepare a list of styleId's as string for easy access
-}
-
-public String getStylesString(){
+  
+public String getStylesXML(){
   return this.stylesString;
+}
+
+public void setPreStyle(String input){
+    this.preStyles=input;
+}
+
+private String getPreStyle(){
+  return this.preStyles;
 }
 
 public String getSummaryStylesString(){
@@ -45,11 +53,6 @@ public String getSummaryStylesString(){
 
 public void setSummaryStylesString(String input){
   this.summaryStyleString=input;
-}
-
-//add a new style to existing Style List
-public void addStyle(xstyle input){
-	this.styleList.add(input);
 }
 
 //swap in a new style list
@@ -88,14 +91,6 @@ public ArrayList<xstyle> getOutline1Styles(){
     return this.outline1Styles;
 }
 
-public void setPreStyle(String input){
-    this.preStyles=input;
-}
-
-public String getPreStyle(){
-  return this.preStyles;
-}
-
 /* 
 Find style tags in styles.xml and return as a String array.
 Then create array of 'xstyle' objects to hold elements.
@@ -116,101 +111,11 @@ public void convertStylesXMLtoObjects(){
     	 currentStyle.setStyleXML(item);
     	 addStyle(currentStyle);
     }
-    extractPreStyles(); //for each new Styles XML save the preparatory section
+    //extractPreStyles(); //for each new Styles XML save the preparatory section
     //System.out.println("Extracted pre styles");
   }
 
-private void checkAndAddStyles() {
-    String newStyles="";
-    Boolean noteTest=false;
-    Boolean codeTest=false;
-    Boolean nrTest=false;
-    for (xstyle currentStyle : this.styleList) {
-       String thisId=currentStyle.getId();
-       System.out.println(thisId);
-       if (thisId.equals("Note")) {
-        noteTest=true;
-       }
-       if (thisId.equals("Code")) {
-        codeTest=true;
-       }
-       if (thisId.equals("NormalReally")) {
-        nrTest=true;
-       }
-    }
-    
-    if (codeTest==false){
-      addCodeStyle();
-    }
-    if (noteTest==false){
-      addNoteStyle();
-    }
-    if (nrTest==false){
-      addNormalReallyStyle();
-    }
-    //update styles if necessary
-    if (codeTest==false || noteTest==false || nrTest==false) {
-      newStyles=recreateStylesFile();
-      updateSummaryStylesString(newStyles);
-    }
-    //System.out.println(newStyles);
-    //System.exit(0);
-}
-
-//TO DO: custom parameter setter for xstyle objects
-//<w:aliases w:val="RCode,PythonCode"/>
-////Source Code F8F8F8 not 00E2AA
-public void addCodeStyle(){
-  String codeStyle = "<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Code\"><w:name w:val=\"Code\"/><w:aliases w:val=\"RCode,PythonCode\"/><w:basedOn w:val=\"Indent1\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\"F8F8F8\"/></w:pPr><w:rPr><w:i/><w:rFonts ascii=\"Consolas\" w:hAnsi=\"Consolas\"</w:rPr></w:style>";
-  xstyle newStyle = new xstyle();
-  newStyle.setStyleXML(codeStyle);
-  addStyle(newStyle); //add to current list of styles.
-  //System.out.println("Code style added");
-  //System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
-}
-
-public void addNoteStyle(){
-  String codeStyle = "<w:style w:type=\"paragraph\" w:customStyle=\"1\" w:styleId=\"Note\"><w:name w:val=\"Note\"/><w:basedOn w:val=\"Indent1\"/><w:autoRedefine/><w:qFormat/><w:pPr><w:shd w:val=\"clear\" w:color=\"auto\" w:fill=\"E3D200\"/></w:pPr><w:rPr><w:i/></w:rPr><w:outlineLvl w:val=\"4\"/></w:style>";
-  xstyle newStyle = new xstyle();
-  newStyle.setStyleXML(codeStyle);
-  addStyle(newStyle); //add to current list of styles.
-  //System.out.println("Note style added");
-  //System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
-}
-
-public void addNormalReallyStyle() {
-  /*
-  String codeStyle="<w:style w:type=\"paragraph\" w:styleId=\"NormalReally\"><w:name w:val=\"NormalReally\"/><w:basedOn w:val=\"Normal\"/><w:uiPriority w:val=\"0\"/><w:qFormat/><w:pPr><w:jc w:val=\"both\"/><w:outlineLvl w:val=\"4\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Times New Roman\" w:eastAsia=\"Times New Roman\" w:hAnsi=\"Times New Roman\" w:cs=\"Times New Roman\"/><w:sz w:val=\"22\"/><w:lang w:bidi=\"ar-SA\"/></w:rPr></w:style>";
-  */
-  xstyle newStyle = new xstyle();
-  newStyle.setStyleAttrib("NormalReally",4,"Times New Roman",10,""); //id, outline,font,size, shade
-  addStyle(newStyle); //add to current list of styles (via XML string).
-  /*
-  System.out.println("Normal Really style added");
-  System.out.println(newStyle.getId()+":"+newStyle.getStyleXML());
-  System.exit(0);
-  */
-}
-
-//<?xml 
-public void extractPreStyles(){
-    String contents = this.stylesString;
-    String starttag="<?xml";  //start of the xml file.
-    String endtag="<w:style "; //end at the first style tag.
-    xmlTool myP = new xmlTool();
-    
-    ArrayList<String> result=myP.getTagAttribInclusive(contents,starttag,endtag);
-    if (result.size()>=0) {
-      preStyles=result.get(0); //get first array item as the string
-    }
-    System.out.println("result:"+result.toString());
-    System.out.println("starttag:"+starttag);
-    String subs=preStyles.substring(0,preStyles.length()-endtag.length());
-    setPreStyle(subs);
-    System.out.println("Start of style file:");
-    System.out.println(getPreStyle());
-}
-
+//prepare a list of styleId's, based on styleIdObjects, as string for easy access
 private void makeSummaryStylesList(){
   ArrayList<xstyle> styleList = getStyleObjects();
   String output="";
@@ -259,8 +164,6 @@ public void setOutlineNames() {
       setOutline1Styles(output1);
 }
 
-
-
 //For loading external list of styles (if needed)
 public void loadDefaultStylesDB(){
   //External list of styles
@@ -281,33 +184,26 @@ public void loadDefaultStylesDB(){
   }
 }
 
-//For loading external list of styles (if needed)
-//input file must have .xml extension
-//This loads from the dbstyles folder (may need a File Chooser program)
-//This should work with an XMLStyles Object, so make it external.
-
-public ArrayList<xstyle>loadNewStylesDB(String inputstyle){
-  //External list of styles
-  String filename = "dbstyles/"+inputstyle;; //file with a list of xml coded styles
-  File myFile = new File(filename);
-  ZipUtil myTool = new ZipUtil();
-  String stylestext=myTool.getFileText(myFile);
-  
-  xmlTool myTag = new xmlTool();
-  String start = "<w:style ";
-  String end = "</w:style>";
-  ArrayList<String> myStylesStrings = myTag.getTagAttribInclusive(stylestext,start,end);
-  ArrayList<xstyle> myStyleObjects = new ArrayList<xstyle>();
-  for (String item : myStylesStrings) {
-    xstyle myNewItem = new xstyle();
-    myNewItem.setStyleXML(item); //this will extract and set relevant name, id inside object automatically.
-    myStyleObjects.add(myNewItem); 
-  }
-  return myStyleObjects; //return, do not specify use
+private void extractPreStyles(){
+    String contents = this.stylesString;
+    String starttag="<?xml";  //start of the xml file.
+    String endtag="<w:style "; //end at the first style tag.
+    xmlTool myP = new xmlTool();
+    
+    ArrayList<String> result=myP.getTagAttribInclusive(contents,starttag,endtag);
+    if (result.size()>=0) {
+      preStyles=result.get(0); //get first array item as the string
+    }
+    System.out.println("result:"+result.toString());
+    System.out.println("starttag:"+starttag);
+    String subs=preStyles.substring(0,preStyles.length()-endtag.length());
+    setPreStyle(subs);
+    System.out.println("Start of style file:");
+    System.out.println(getPreStyle());
 }
 
 
-public String recreateStylesFile(){
+public void recreateStylesFile(){
   String output=getPreStyle();
   ArrayList <xstyle> myStyleList = getStyleObjects();
   for (xstyle item : myStyleList) {
@@ -316,7 +212,23 @@ public String recreateStylesFile(){
   output=output+"</w:styles>";
   System.out.println(output);
   //System.exit(0);
-  return output;
+  //return output;
+  //update here
+  this.stylesString=output;
+  setOutlineNames();
+  updateSummaryStylesString();
+}
+
+//helper for logging
+public void logging(){
+   ArrayList<xstyle> myStyleList = getStyleObjects();
+   for (xstyle updatedStyle : myStyleList) {
+      System.out.println(updatedStyle.getId());
+  }
+  System.out.println("\n Style String...");
+  System.out.println(getStylesXML());
+  System.out.println("\n Now for summary...");
+  System.out.println(getSummaryStylesString());
 }
 
 }
